5557bbcd17c9fd0184c468579c2ef9c636e493ae
NyeinKoKoAung <141461103+NyeinKoKoAung@users.noreply.github.com>
2023-09-09 04:41:49 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ plain='\033[0m'
 
 #if [[ -n $4 ]] && [[ $(($(date +%s) - $4)) -lt 120 ]] && [[ $(($(date +%s) - $4)) -ge 0 ]]; then
 
-sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
+sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
 echo "<h3><font color='red'>▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬
 </font></h3>
 <h3><font color='#f40125'>--- ۩ PREMIUM SERVER ۩ ---
@@ -24,7 +24,7 @@ echo "<h3><font color='red'>▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬
 <h2><font color='#F535AA'>Telegram Channel >> https://t.me/nkka_404
 </font></h3>
 <h3><font color='red'>▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬
-</font></h3>" | tee /etc/ssh >/dev/null
+</font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
 useradd "nkka404" --shell=/bin/false -M
 echo "nkka404:nkka404" | chpasswd
 
