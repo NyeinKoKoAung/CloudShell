61be6357b24b9c20e9e3789791ee57b06703e1be
NyeinKoKoAung <141461103+NyeinKoKoAung@users.noreply.github.com>
2023-09-12 03:28:49 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,17 +17,20 @@ none='\e[0m'
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
 echo "
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h3><font color='red'>
+▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬
 </font></h3>
-<h3><font color='magenta'>--- ۩ PREMIUM SERVER ۩ ---
-</font></h3>
-<h3><font color='green'>--- ۩ SERVER BY ④ ⍤⃝🌻④ ۩ ---
-</font></h3>
-<h4><font color='cyan'>Telegram Channel >> https://t.me/Pmttg
-</font></h4>
-<h4><font color='blue'>Telegram Channel >> https://t.me/nkka_404
-</font></h4>
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h6><font color='magenta'>
+___   ___        ________       ___   ___     
+|\  \ |\  \      |\   __  \     |\  \ |\  \    
+\ \  \|_\  \     \ \  \|\  \    \ \  \|_\  \   
+ \ \______  \     \ \  \/\  \    \ \______  \  
+  \|_____|\  \     \ \  \/\  \    \|_____|\  \ 
+         \ \__\     \ \_______\          \ \__\
+          \|__|      \|_______|           \|__|
+</font></h6>
+<h3><font color='red'>
+▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
 useradd "404" --shell=/bin/false -M
 echo "404:404" | chpasswd
